2cef60e8a8bdfbba5276e85d32d5442d4573171b
sreesindhusruthiyadavalli <sssindh@gmail.com>
2018-12-04 17:26:09 +0530

    creating traning and serving jobs

--- variable.bash
+++ variable.bash
@@ -16,3 +16,18 @@ CISCOAI_GITHUB_VERSION=master
 
 ## Name of the NFS Persistent Volume
 NFS_PVC_NAME=nfs
+
+## Used in training.bash
+# Enviroment variables for mnist training job (See mnist_model.py)
+TF_DATA_DIR=/mnt/data
+TF_MODEL_DIR=/mnt/model
+NFS_MODEL_PATH=/mnt/export
+TF_EXPORT_DIR=${NFS_MODEL_PATH}
+
+# If you want to use your own image,
+# make sure you have a dockerhub account and change
+# DOCKER_BASE_URL and IMAGE below.
+DOCKER_BASE_URL=docker.io/sryadava
+IMAGE=${DOCKER_BASE_URL}/tf-mnist-demo:v3
+#docker build . --no-cache  -f Dockerfile -t ${IMAGE}
+#docker push ${IMAGE}
